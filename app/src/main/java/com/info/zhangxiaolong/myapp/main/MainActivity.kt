65337f4c27b7e5c880b3fe65bff3125da7fcfa1b
zhangxiaolong <zhangxiaolong.android@bytedance.com>
2018-02-19 23:21:39 +0800

    init commit

---
+++ app/src/main/java/com/info/zhangxiaolong/myapp/main/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.info.zhangxiaolong.myapp.main
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.info.zhangxiaolong.myapp.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
