20f22ec546e8545bcfb5374594f0ec596438d236
zhangxiaolong <zhangxiaolong.android@bytedance.com>
2018-02-22 23:54:22 +0800

    tmp commit

--- app/src/main/java/com/info/zhangxiaolong/myapp/main/fragment/HomeFragment.kt
+++ app/src/main/java/com/info/zhangxiaolong/myapp/main/fragment/HomeFragment.kt
@@ -1,7 +1,9 @@
 package com.info.zhangxiaolong.myapp.main.fragment
 
 import android.os.Bundle
+import android.support.design.widget.TabLayout
 import android.support.v4.app.Fragment
+import android.support.v4.view.ViewPager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -12,7 +14,22 @@ import com.info.zhangxiaolong.myapp.R
  */
 class HomeFragment : Fragment() {
 
+    private var tabNameContainer : TabLayout? = null
+    private var tabContentContainer : ViewPager? = null
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.home_page_layout , container , false)
+        val view = inflater.inflate(R.layout.home_page_layout , container , false)
+        tabNameContainer = view.findViewById(R.id.tab_name_container)
+        tabContentContainer = view.findViewById(R.id.tab_content_container)
+
+        //init
+        tabNameContainer?.let {
+            //添加频道
+            it.addTab(it.newTab().setText(resources.getText(R.string.channel_zhihu)))
+            it.addTab(it.newTab().setText(resources.getText(R.string.channel_douban)))
+            it.addTab(it.newTab().setText(resources.getText(R.string.channel_guoke)))
+            it.addTab(it.newTab().setText(resources.getText(R.string.channel_beauty)))
+        }
+        return view
     }
 }
